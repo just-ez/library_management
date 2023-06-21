d05ad73164759db94597911fda75b35907e399a9
kingofvibe <vibecity112@gmail.com>
2023-06-21 15:31:33 +0100

    initial commit

---
+++ library_management/library_management/doctype/library_settings/library_settings.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Ezra and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class LibrarySettings(Document):
+	pass
